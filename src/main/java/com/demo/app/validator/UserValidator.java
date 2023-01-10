ec1e34622953fdb1d590c19284566022a20fd71a
Kulbhushan <kulbhushan.mayer@gmail.com>
2023-01-10 12:30:38 +0530

    updated version and fixed duplicate literals

--- src/main/java/com/demo/app/validator/UserValidator.java
+++ src/main/java/com/demo/app/validator/UserValidator.java
@@ -22,14 +22,14 @@ public class UserValidator implements Validator {
 	@Override
 	public void validate(Object o, Errors errors) {
 		User user = (User) o;
-
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username",
+		String username = "username";
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, username,
 				"NotEmpty");
 		if (user.getUsername().length() < 6 || user.getUsername().length() > 32) {
-			errors.rejectValue("username", "Size.userForm.username");
+			errors.rejectValue(username, "Size.userForm.username");
 		}
 		if (userService.findByUsername(user.getUsername()) != null) {
-			errors.rejectValue("username", "Duplicate.userForm.username");
+			errors.rejectValue(username, "Duplicate.userForm.username");
 		}
 
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password",
